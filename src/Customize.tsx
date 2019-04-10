f1972a13642908d4c45eb1b8c05b8fd8ebc30a87
Matt Karl <matt@mattkarl.com>
2019-04-10 15:58:49 -0700

    Adds syntax highlighting and group select all/none

--- src/Customize.tsx
+++ src/Customize.tsx
@@ -1,8 +1,9 @@
 import { h, Component } from 'preact';
 import logoUrl from './images/logo.svg';
 import bind from 'bind-decorator';
+import { HighLight } from 'preact-highlight';
 import { createBundleCode } from './CodeUtils';
-import { packagesData, packagesMap, defaultPackages, Package } from './PackagesData';
+import { packagesData, packagesMap, defaultPackages, Package, PackageGroup } from './PackagesData';
 
 interface State {
     packages:string[];
@@ -45,6 +46,29 @@ export class Customize extends Component<any, State> {
         else {
             packages.splice(packages.indexOf(name), 1);
         }
+        this.refreshPackages(packages);
+    }
+
+    private onToggleGroup(group:PackageGroup, event:Event) {
+        console.log('toggle group', group);
+        const {checked} = event.currentTarget as HTMLInputElement;
+        const {packages} = this.state;
+        if (checked) {
+            group.packages
+                .filter(name => !packagesMap[name].required)
+                .filter(name => !packages.includes(name))
+                .forEach(name => packages.push(name));
+        }
+        else {
+            group.packages
+                .filter(name => !packagesMap[name].required)
+                .filter(name => packages.includes(name))
+                .forEach(name => packages.splice(packages.indexOf(name), 1));
+        }
+        this.refreshPackages(packages);
+    }
+
+    private refreshPackages(packages:string[]) {
         localStorage.setItem('packages', JSON.stringify(packages));
         this.setState({ packages, bundleCode: createBundleCode(packages) });
     }
@@ -62,6 +86,15 @@ export class Customize extends Component<any, State> {
         this.setState({ useYarn });
     }
 
+    private groupSelected(group:PackageGroup): boolean {
+        for (const name of group.packages) {
+            if (!this.state.packages.includes(name)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     render(props:any, { packages, useYarn, bundleCode }:State) {
         return (<div class="app-container">
             <div class="app-header">
@@ -75,9 +108,16 @@ export class Customize extends Component<any, State> {
             </div>
             <div class="app-main row">
                 <div class="app-col col-sm-4 col-md-3">
-                    { packagesData.groups.map(group => {
+                    { packagesData.groups.map((group, i) => {
                         return <div class="customize-group">
-                            <h2>{group.title}</h2>
+                            <h2><div class="custom-control custom-checkbox">
+                                <input type="checkbox"
+                                    class="custom-control-input"
+                                    id={`package_group${i}`}
+                                    checked={this.groupSelected(group)}
+                                    onChange={this.onToggleGroup.bind(this, group)} />
+                                <label class="custom-control-label" for={`package_group${i}`}>{group.title}</label>
+                            </div></h2>
                             <ul class="customize-list-group">
                                 { group.packages.map(name => packagesMap[name]).map(pkg => {
                                     return <li class={`customize-list-group-item ${pkg.required ? 'disabled' : ''}`}>
@@ -98,11 +138,9 @@ export class Customize extends Component<any, State> {
                 </div>
                 <div class="app-col col-sm-4 col-md-6">
                     <h2>PIXI Export <small class="text-secondary float-right">pixi-custom.js</small></h2>
-                    <code class="customize-code preformatted mb-4">{bundleCode}</code>
+                    <HighLight className="customize-code" code={bundleCode} language="javascript" />
                     <h2>PIXI Import<small class="text-secondary float-right">index.js</small></h2>
-                    <code class="customize-code">
-                        import * as PIXI from './pixi-custom.js';
-                    </code>
+                    <HighLight className="customize-code" code="import * as PIXI from './pixi-custom.js';" language="javascript" />
                 </div>
                 <div class="app-col col-sm-4 col-md-3">
                     <h2>Install</h2>

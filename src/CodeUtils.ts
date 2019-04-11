e7f4f64c2ce1410bc1c590b5d7d801c7b5ed767e
Matt Karl <matt@mattkarl.com>
2019-04-10 19:03:59 -0700

    Add browser code generation

--- src/CodeUtils.ts
+++ src/CodeUtils.ts
@@ -37,17 +37,13 @@ function rendererPlugin(rendererName:string, {name, rendererPlugin, canvasPlugin
 }
 
 /**
- * Generate code for application plugins
+ * Generate code for renderer plugins
  */
-function appPlugin({name, appPlugin}:Package) {
-    return [`Application.registerPlugin(${appPlugin})`];
-}
-
-/**
- * Generate code for loader plugins
- */
-function loaderPlugin({name, loaderPlugin}:Package) {
-    return [`Loader.registerPlugin(${loaderPlugin})`];
+function rendererHTMLPlugin(rendererName:string, {name, rendererPlugin, canvasPlugin, namespace}:Package) {
+    const target = rendererPlugin || canvasPlugin;
+    const [apiName, className] = target;
+    namespace = namespace ? `${namespace}.` : '';
+    return `PIXI.${rendererName}.registerPlugin('${apiName}', PIXI.${namespace}${className});`;
 }
 
 /**
@@ -85,12 +81,12 @@ export function createBundleCode(packages:string[]) {
 
     if (packages.includes('@pixi/app') && appPlugins.length) {
         lines.push('', '// Application plugins');
-        appPlugins.forEach(pkg => lines.push.apply(lines, appPlugin(pkg)));
+        appPlugins.forEach(pkg => lines.push(`Application.registerPlugin(${pkg.appPlugin})`));
     }
 
     if (packages.includes('@pixi/loaders') && loaderPlugins.length) {
         lines.push('', '// Loader plugins');
-        loaderPlugins.forEach(pkg => lines.push.apply(lines, loaderPlugin(pkg)));
+        loaderPlugins.forEach(pkg => lines.push(`Loader.registerPlugin(${pkg.loaderPlugin})`));
     }
 
     if (filters.length) {
@@ -107,4 +103,44 @@ export function createBundleCode(packages:string[]) {
     return lines
         .map(line => line && !line.startsWith('//') ? `${line};` : line)
         .join('\n');
+}
+
+/**
+ * Generate the HTML code
+ */
+export function createHTMLCode(packages:string[]) {
+    const lines = packages.map(name => {
+        const [ns, n] = name.split('/');
+        return `<script src="https://pixijs.download/dev/packages/${n}.min.js"></script>`;
+    });
+
+    const loaderPlugins = packagesData.packages
+        .filter(pkg => packages.includes(pkg.name) && !!pkg.loaderPlugin);
+    const appPlugins = packagesData.packages
+        .filter(pkg => packages.includes(pkg.name) && !!pkg.appPlugin);
+    const filters = packagesData.packages
+        .filter(pkg => packages.includes(pkg.name) && !!pkg.filter);
+    const canvasPlugins = packagesData.packages
+        .filter(pkg => packages.includes(pkg.name) && !!pkg.canvasPlugin);
+
+    lines.push('<script>');
+    const TAB = '  ';
+    packagesData.packages
+        .filter(pkg => packages.includes(pkg.name) && !!pkg.rendererPlugin)
+        .forEach(pkg => lines.push(TAB + rendererHTMLPlugin('Renderer', pkg)));
+
+    if (packages.includes('@pixi/canvas-renderer') && canvasPlugins.length) {
+        canvasPlugins.forEach(pkg => lines.push(TAB + rendererHTMLPlugin('CanvasRenderer', pkg)));
+    }
+
+    if (packages.includes('@pixi/app') && appPlugins.length) {
+        appPlugins.forEach(pkg => lines.push(TAB + `PIXI.Application.registerPlugin(PIXI.${pkg.appPlugin});`));
+    }
+
+    if (packages.includes('@pixi/loaders') && loaderPlugins.length) {
+        loaderPlugins.forEach(pkg => lines.push(TAB + `PIXI.Loader.registerPlugin(PIXI.${pkg.loaderPlugin});`));
+    }
+
+    lines.push('</script>');
+    return lines.join('\n');
 }
--- src/Customize.tsx
+++ src/Customize.tsx
@@ -3,13 +3,15 @@ import logoUrl from './images/logo.svg';
 import downloadUrl from './images/download.svg';
 import bind from 'bind-decorator';
 import { HighLight } from 'preact-highlight';
-import { createBundleCode } from './CodeUtils';
+import { createBundleCode, createHTMLCode } from './CodeUtils';
 import { packagesData, packagesMap, defaultPackages, Package, PackageGroup } from './PackagesData';
 
 interface State {
     packages:string[];
     useYarn:boolean;
+    useBrowser:boolean;
     bundleCode?:string;
+    htmlCode?:string;
 }
 
 /**
@@ -22,7 +24,7 @@ export class Customize extends Component<any, State> {
         const state:State = {
             packages: defaultPackages,
             useYarn: !!localStorage.getItem('useYarn'),
-            bundleCode: ''
+            useBrowser: !!localStorage.getItem('useBrowser')
         };
 
         // Check for saved packages
@@ -31,6 +33,7 @@ export class Customize extends Component<any, State> {
             state.packages = JSON.parse(savedPackages);
         }
         state.bundleCode = createBundleCode(state.packages);
+        state.htmlCode = createHTMLCode(state.packages);
         this.state = state;
     }
 
@@ -69,9 +72,16 @@ export class Customize extends Component<any, State> {
         this.refreshPackages(packages);
     }
 
+    /**
+     * Regenerate the bundle source code
+     */
     private refreshPackages(packages:string[]) {
         localStorage.setItem('packages', JSON.stringify(packages));
-        this.setState({ packages, bundleCode: createBundleCode(packages) });
+        this.setState({
+            packages,
+            bundleCode: createBundleCode(packages),
+            htmlCode: createHTMLCode(packages)
+        });
     }
 
     /**
@@ -87,6 +97,19 @@ export class Customize extends Component<any, State> {
         this.setState({ useYarn });
     }
 
+    /**
+     * Handle browser type
+     */
+    private onBrowser(useBrowser:boolean) {
+        if (useBrowser) {
+            localStorage.setItem('useBrowser', '1');
+        }
+        else {
+            localStorage.removeItem('useBrowser');
+        }
+        this.setState({ useBrowser });
+    }
+
     private groupSelected(group:PackageGroup): boolean {
         for (const name of group.packages) {
             if (!this.state.packages.includes(name)) {
@@ -96,10 +119,16 @@ export class Customize extends Component<any, State> {
         return true;
     }
 
-    render(props:any, { packages, useYarn, bundleCode }:State) {
+    render(props:any, { packages, useYarn, useBrowser, bundleCode, htmlCode }:State) {
         return (<div class="app-container">
             <div class="app-header">
                 <header class="mb-2">
+                    <div class="btn-group m-2 float-right">
+                        <button class={`btn btn-sm px-3 btn-${useBrowser ? 'primary' : 'outline-secondary'}`}
+                            onClick={this.onBrowser.bind(this, true)}>Browser</button>
+                        <button class={`btn btn-sm px-3 btn-${!useBrowser ? 'primary' : 'outline-secondary'}`}
+                            onClick={this.onBrowser.bind(this, false)}>Bundler</button>
+                    </div>
                     <h1><img src={logoUrl} class="logo" alt="PixiJS" /> Customize</h1>
                 </header>
             </div>
@@ -133,32 +162,41 @@ export class Customize extends Component<any, State> {
                         </div>;
                     }) }
                 </div>
-                <div class="app-col col-sm-4 col-md-6">
-                    <h2>Bundle Code
-                        <a download="pixi.js" href={`data:text/plain,${bundleCode}`}>
-                            <button class="btn btn-sm btn-primary px-3 float-right">
-                                <img src={downloadUrl} class="mr-2" width="20" height="20" />
-                                Download
-                            </button>
-                        </a>
-                    </h2>
+                { !useBrowser && <div class="app-col app-col-main col-sm-4 col-md-6">
+                    <h2>Bundle Code</h2>
                     <p>When using <a href="https://webpack.js.org/">Webpack</a>,
                     <a href="https://rollupjs.org"> Rollup</a> or
                     <a href="https://parceljs.org/"> Parcel</a> you can embed
                     the follow code in your project and then
                     simply <code>import * as PIXI from './pixi.js'</code>.</p>
-                    <HighLight className="customize-code" code={bundleCode} language="javascript" />
-                </div>
-                <div class="app-col col-sm-4 col-md-3">
+                    <HighLight className="customize-code mb-2" code={bundleCode} language="javascript" />
+                    <a download="pixi.js" href={`data:text/plain,${bundleCode}`}>
+                        <button class="btn btn-primary btn-block float-right">
+                            <img src={downloadUrl} class="mr-2" width="20" height="20" />
+                            Download
+                        </button>
+                    </a>
+                </div> }
+                { !useBrowser && <div class="app-col col-sm-4 col-md-3">
                     <h2>Install</h2>
                     <div class="btn-group w-100 mb-2">
                         <button class={`btn btn-sm btn-${!useYarn ? 'primary' : 'outline-secondary'}`} onClick={this.onYarn.bind(this, false)}>npm</button>
                         <button class={`btn btn-sm btn-${useYarn ? 'primary' : 'outline-secondary'}`} onClick={this.onYarn.bind(this, true)}>yarn</button>
                     </div>
-                    <code class="customize-code mb-4">
+                    <code class="customize-code mb-4 small">
                         { useYarn ? 'yarn add' : 'npm install'} { packages.join(' ') }
                     </code>
-                </div>
+                </div> }
+                { useBrowser && <div class="app-col app-col-main col-sm-8 col-md-9">
+                    <h2>Browser Code</h2>
+                    <HighLight className="customize-code mb-2" code={htmlCode} language="html" />
+                    <a download="pixi.html" href={`data:text/plain,${htmlCode}`}>
+                        <button class="btn btn-primary btn-block float-right">
+                            <img src={downloadUrl} class="mr-2" width="20" height="20" />
+                            Download
+                        </button>
+                    </a>
+                </div> }
             </div>
         </div>);
     }
